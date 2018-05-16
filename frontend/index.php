52c56043151f5f793af817f77ec3a599b9ed66c3
Ivan Hrebenyk <34397651+IvanHrebenyk@users.noreply.github.com>
2018-05-16 19:53:32 +0300

    header menu for moderator

--- frontend/index.php
+++ frontend/index.php
@@ -1,7 +1,9 @@
 <?
 include "head.php";
 echo '<header>'; 
-include "header_user.php"; 
+//include "header_user.php"; 
+include "header_moderator.php"; 
+//include "header_admin.php"; 
 echo '</header>';
 echo '<footer >';
 echo '<div class="navbar-fixed-bottom row-fluid"><div class="navbar-inner"><div class="container" >';
--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,4 @@
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-})
+function messege_delete_info_system(){
+	alert("Для видалення компоненту з системи, перейдіть на сторінку з повним описом компоненту та натисніть кнопку \"Видалення\"");
+}
+
