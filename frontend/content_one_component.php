7c5b1857e1e8e1f3a0421b9213bdb201000eab8b
Ivan Hrebenyk <34397651+IvanHrebenyk@users.noreply.github.com>
2018-05-16 23:49:23 +0300

    Addition style and web page

---
+++ frontend/content_one_component.php
@@ -0,0 +1,69 @@
+<div class="container" style="bacground-color: #fafafa;">
+	<div class="row">
+		<div class="col-2">
+			<button class="btn btn-default btn-xs go-back" style="padding: 0px; float: left;"><a href="#" class="btn btn-default"  >Назад до списку</a></button>
+		</div>
+		<div class="col-8"></div>
+		<div class="col-2">
+			<button class="btn btn-default btn-xs delete-component" style="padding: 0px; float: right;"><a href="#" class="btn btn-default"  >Видалити</a></button>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-4"></div>
+		<div class="col-4" align="center">
+			<a href="test.jpg">
+				<img src="test.jpg" class="img-thumbnail rounded " title="Компонент" width="330px">
+			</a>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-3"></div>
+		<div class="col-6" style="text-align: center; font-family: Consolas;">
+			<h5>Nvidia GTX-1070 power solution</h5>
+			<button class="btn btn-default btn-xs get-component" type="button" data-toggle="modal" data-target="#open-category-component" id="button-component">Підібрати компоненти</button>
+			<?include "modal_component.php";?>
+		</div>
+		<div class="col-3"></div>
+	</div>
+</div>
+<style>
+
+</style>
+<div class="container-fluid" style="margin-top: 40px;margin-bottom: 40px;">
+	<div class="row">
+		<div class="col-6">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-6" ><h6>Характеристика 1:</h6></div>
+					<div class="col-6" >
+						<span class="option-component-label">Показник 1;</span>
+					</div>
+				</div>
+				<hr>
+			</div>
+		</div>
+		
+		
+		<div class="col-6">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-6" ><h6>Характеристика 2:</h6></div>
+					<div class="col-6" >
+						<span class="option-component-label">Показник 2;</span>
+					</div>
+				</div>
+				<hr>
+			</div>
+		</div>
+	</div>
+	
+	
+	
+</div>
+
--- frontend/index.php
+++ frontend/index.php
@@ -1,10 +1,15 @@
 <?
 include "head.php";
 echo '<header>'; 
-//include "header_user.php"; 
-include "header_moderator.php"; 
+include "header_user.php"; 
+//include "header_moderator.php"; 
 //include "header_admin.php"; 
 echo '</header>';
+////////////// body content ////////
+echo '<div class="content-info">';
+include "content_one_component.php";
+echo '</div>';
+////////////// body content ////////
 echo '<footer >';
 echo '<div class="navbar-fixed-bottom row-fluid"><div class="navbar-inner"><div class="container" >';
 	include "footer.php";
